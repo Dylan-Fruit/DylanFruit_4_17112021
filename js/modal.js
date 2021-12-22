9ab2e2e475f017cebb05ac4db12d22dab4de1d75
Dylan Fruit <dylanfruit@outlook.fr>
2021-12-22 14:43:42 +0100

    form validatation: ok, change of conditions

--- js/modal.js
+++ js/modal.js
@@ -30,6 +30,15 @@ const town = document.querySelector(".town");
 const townForm = document.querySelector(".townForm");
 const conditions = document.querySelector(".conditions");
 const conditionsForm = document.querySelector(".conditionsForm");
+// Appel de toutes les erreurs 
+const firstErr = document.querySelector(".firstErr");
+const lastErr = document.querySelector(".lastErr");
+const emailErr = document.querySelector(".emailErr");
+const birthdateErr = document.querySelector(".birthdateErr");
+const quantityErr = document.querySelector(".quantityErr");
+const townErr = document.querySelector(".townErr");
+const conditionsErr = document.querySelector(".conditionsErr");
+
 
 
 // launch modal event
@@ -44,111 +53,111 @@ function launchModal() {
 
 // close modal event 
 closeBtn.forEach((close) => close.addEventListener("click", closeModal));
-closeAfterValid.addEventListener("click", closeModal);
+closeAfterValid.addEventListener("click", function(){
+  form.submit()
+});
 
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
 
+// Soumission du formulaire 
 modalSubmit.addEventListener('click', function(e){
   e.preventDefault();
-  if(form.style.display !== "none"){
-    submitSuccess.style.display = "block";
-    form.style.display = "none"
+  validFirst();
+  validLast();
+  validEmail();
+  validBirthdate();
+  validQuantity();
+  validTown();
+  validConditions();
+  if(firstValid && lastValid && emailValid && birthdateValid && quantityValid && townValid && conditionsValid === true){
+      console.log('test');
+      form.style.display = "none";
+      submitSuccess.style.display = "block";
   }
 });
 
-
-// Soumission du formulaire 
-
 // Validation Prénom 
-firstName.addEventListener('change', function(){
-  validFirst(this);
+firstName.addEventListener('focusout', function(){
+  validFirst();
 });
 
 // création d'un paragraphe dans le HTML pour le message d'erreur
-let firstError = document.createElement("p");
+//firstErr = document.createElement('p');
+let firstValid;
+firstErr.style.display = "none";
 
-const validFirst = function(inputFirst){
-  let msg; 
+function validFirst(){
   //Au moins 2 caractères dans le prénom 
-  if(inputFirst.value.length < 2){
-    firstNameForm.appendChild(firstError);
-    msg = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
-    firstError.innerHTML = msg;
-    firstError.classList.add('text-danger');
+  if(firstName.value.length < 2){
+    firstErr.style.display = "block";
+    firstErr.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     firstName.style.border = "solid 2px red";
-    return false;
+    firstValid = false;
   } else {
-      firstError.classList.remove('text-danger');
+      firstErr.style.display = "none";
       firstName.style.border = "none";
-      firstNameForm.removeChild(firstError);
-      return true;
+      firstValid = true;
   }
 }
 
+
 // Validation Nom
 lastName.addEventListener('change', function(){
-  validLast(this);
+  validLast();
 });
 
-// création d'un paragraphe dans le HTML pour le message d'erreur
-let lastError = document.createElement("p");
+let lastValid;
+lastErr.style.display = "none";
 
-const validLast = function(inputLast){
-  let msg; 
+function validLast(){ 
   // Au moins 2 caractères dans le nom 
-  if(inputLast.value.length < 2){
-    lastNameForm.appendChild(lastError);
-    msg = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
-    lastError.innerHTML = msg;
-    lastError.classList.add('text-danger');
+  if(lastName.value.length < 2){
+    lastErr.style.display = "block";
+    lastErr.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     lastName.style.border = "solid 2px red";
-    return false;
+    lastValid = false;
   } else {
-      lastError.classList.remove('text-danger');
+      lastErr.style.display = "none";
       lastName.style.border = "none";
-      lastNameForm.removeChild(lastError);
-      return true;
+      lastValid = true;
   }
 }
 
 // Validation email 
 email.addEventListener('change', function(){
-  validEmail(this);
+  validEmail();
 });
 
 // Regex pour l'email
 let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
 
 // création d'un paragraphe dans le HTML pour le message d'erreur
-let emailError = document.createElement("p");
+let emailValid;
+emailErr.style.display = "none";
 
-const validEmail = function(inputEmail){
-  let msg; 
+function validEmail(){
   // Adresse valide obligatoire  
-  if(emailRegExp.test(inputEmail.value)){
-    emailError.classList.remove('text-danger');
+  if(emailRegExp.test(email.value)){
+    emailErr.style.display = "none";
     email.style.border = "none";
-    emailForm.removeChild(emailError);
-    return true;
+    emailValid = true;
   } else {
-    emailForm.appendChild(emailError);
-    msg = 'Veuillez entrer une adresse email valide.';
-    emailError.innerHTML = msg;
-    emailError.classList.add('text-danger');
+    emailErr.style.display = "block";
+    emailErr.textContent = "Veuillez entrer une adresse email valide.";
     email.style.border = "solid 2px red";
-    return false;
+    emailValid = false;
   }
 }
 
 // Validation date de naissance 
 birthdate.addEventListener('change', function(){
-  validBirthdate(this);
+  validBirthdate();
 });
 
-// La date de naissance ne peut pas être supérieur à 2005
+// La date de naissance ne peut pas être supérieur à 2005 (16 ans max)
 const minDate = new Date('December 31, 2005').toISOString().split('T')[0];
 console.log(minDate);
 birthdate.max = minDate;
@@ -156,104 +165,82 @@ birthdate.max = minDate;
 // Expression regulière au format YYYY-MM-DD
 let birthdateRegEx = new RegExp('^[0-9]{4}-[0-9]{2}-[0-9]{2}$');
 
-let birthdateError = document.createElement("p");
+let birthdateValid;
+birthdateErr.style.display = "none";
 
-const validBirthdate = function(inputBirthdate){
-  let msg; 
-  if(birthdateRegEx.test(inputBirthdate.value)){
-    birthdateError.classList.remove('text-danger');
+function validBirthdate(){
+  if(birthdateRegEx.test(birthdate.value)){
+    birthdateErr.style.display = "none";
     birthdate.style.border = "none";
-    birthdateForm.removeChild(birthdateError);
-    return true;
-    // Erreur dans le cas où quelqu'un écrirait une date supérieure à la limite à la main
-  } else if(inputBirthdate.value > birthdate.max){ 
-    birthdateForm.appendChild(birthdateError);
-    msg = 'Veuillez indiquer une date de naissance valide.';
-    birthdateError.innerHTML = msg;
-    birthdateError.classList.add('text-danger');
-    birthdate.style.border = "solid 2px red";
-    return false;
+    birthdateValid = true;
   } else {
-    birthdateForm.appendChild(birthdateError);
-    msg = 'Veuillez indiquer votre date de naissance.';
-    birthdateError.innerHTML = msg;
-    birthdateError.classList.add('text-danger');
+    birthdateErr.style.display = "block";
+    birthdateErr.textContent = "Veuillez indiquer votre date de naissance."
     birthdate.style.border = "solid 2px red";
-    return false;
+    birthdateValid = false;
   }
 }
 
 
 // Nombres de tournois 
 quantity.addEventListener('change', function(){
-  validQuantity(this);
+  validQuantity();
 });
 
 // RegExp pour les chiffres jusqu'à 6 répétitions max 
 let quantityRegExp = new RegExp('^[0-9]{1,6}$');
 
-// création d'un paragraphe dans le HTML pour le message d'erreur
-let quantityError = document.createElement("p");
+let quantityValid;
+quantityErr.style.display = "none";
 
-const validQuantity = function(inputQuantity){
-  let msg; 
+function validQuantity(){
   // entrer un chiffre  
-  if(quantityRegExp.test(inputQuantity.value)){
-    quantityError.classList.remove('text-danger');
+  if(quantityRegExp.test(quantity.value)){
+    quantityErr.style.display = "none";
     quantity.style.border = "none";
-    quantityForm.removeChild(quantityError);
-    return true;
+    quantityValid = true;
   } else {
-    quantityForm.appendChild(quantityError);
-    msg = 'Vous devez entrer un nombre.';
-    quantityError.innerHTML = msg;
-    quantityError.classList.add('text-danger');
+    quantityErr.style.display = "block";
+    quantityErr.textContent = "Veuillez entrer un nombre."
     quantity.style.border = "solid 2px red";
-    return false;
+    quantityValid = false;
   }
 }
 
 // Validation villes 
 town.addEventListener('change', function(){
-  validTown(this);
+  validTown();
 });
 
-// création d'un paragraphe dans le HTML pour le message d'erreur 
-let townError = document.createElement('p');
+let townValid;
+townErr.style.display = "none";
 
-const validTown = function(inputTown){
-  let msg; 
-  if(inputTown.checked){
-    townError.classList.remove('text-danger');
-    townForm.removeChild(townError);
-    return true;
+function validTown(){
+  if(town.checked){
+    townErr.style.display = "none";
+    townValid = true;
   } else {
-    townForm.appendChild(townError);
-    msg = 'Vous devez choisir une ville';
-    townError.innerHTML = msg;
-    townError.classList.add('text-danger');
-    return false;
+    townErr.style.display = "block";
+    townErr.textContent = "Vous devez choisir une ville.";
+    townValid = false;
   }
 }
 
 // Case des conditions bien cochée 
 conditions.addEventListener('change', function(){
-  validConditions(this);
+  validConditions();
 });
 
-let conditionsError = document.createElement('p');
+let conditionsValid;
+conditionsErr.style.display = "none";
 
-const validConditions = function(inputConditions){
-  let msg;
-  if(inputConditions.checked){
-    conditionsError.classList.remove('text-danger');
-    conditionsForm.removeChild(conditionsError);
-    return true;
+function validConditions(){
+  if(conditions.checked){
+    conditionsErr.style.display = "none";
+    conditionsValid = true;
   } else {
-    conditionsForm.appendChild(conditionsError);
-    msg = 'Vous devez accepter les termes et conditions';
-    conditionsError.innerHTML = msg;
-    conditionsError.classList.add('text-danger');
-    return false;
+    conditionsErr.style.display = "block";
+    conditionsErr.textContent = "Vous devez accepter les termes et conditions pour participer."
+    conditionsValid = false;
   }
 }
